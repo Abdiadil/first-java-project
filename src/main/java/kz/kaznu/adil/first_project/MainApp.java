c5e95859d3a487ffe6da06d98f035f809a2e0cf9
SAMask127node <104855433+SAMask127node@users.noreply.github.com>
2026-09-24 12:38:35 +0500

    fix: add line math calc

--- src/main/java/kz/kaznu/adil/first_project/MainApp.java
+++ src/main/java/kz/kaznu/adil/first_project/MainApp.java
@@ -3,5 +3,6 @@ package kz.kaznu.adil.first_project;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello world from java!");
+        System.out.println(2+6);
     }
 }
